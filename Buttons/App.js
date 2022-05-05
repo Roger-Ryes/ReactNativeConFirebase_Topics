7c7d0c52e2af76f26d9c86136df2778bdc868fa4
Roger-Ryes <roger.reyesm97@gmail.com>
2022-05-04 20:18:50 -0500

    botones

---
+++ Buttons/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default function App() {
+  let nameButton="Mi boton"
+  let greeting = () => {
+    Alert.alert("Title","Hello since funcion");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>BasicApp! app perron</Text>
+      <StatusBar style="auto" />
+
+      <Button title={nameButton}
+              onPress={()=>{ 
+                Alert.alert("Title","Hello word")
+              }}/>
+
+      <Button title="OK"
+              onPress={()=>{ greeting() }}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
